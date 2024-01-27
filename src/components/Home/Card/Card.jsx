9ed5af33467cfba1bf5af0a73760ccf99e1b65f0
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-01-27 13:34:00 +0600

    card object

--- src/components/Home/Card/Card.jsx
+++ src/components/Home/Card/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import cat from "../../../images/cat.png";
-import dog from "../../../images/dog.jpg";
-import rabbit from "../../../images/dog-2.png";
+import dog from "../../../images/dog-2.png";
+import rabbit from "../../../images/rabbit-2.png";
 import "./Card.css";
 
 const Card = () => {
@@ -9,73 +9,58 @@ const Card = () => {
     {
       id: 1,
       name: "Catto",
-      img: { cat },
+      img: cat ,
       description:
         "minus at porro?ssitatibus laborum minus at porro? culpaalias ullam. Necessitatibus, ut placeat i dolorum  assumenda, sint quam Repudi culpa al minus at porro?ssitatibus laborum minu minus at porro?ssitatibus laborum minus at porro? culpas at porro? culpaias",
     },
     {
       id: 2,
       name: "Doggo",
-      img: { dog },
+      img: dog,
       description:
         "minus at porro?ssitatibus laborum minus at porro? culpaalias ullam. Necessitatibus, ut placeat i dolorum  assumenda, sint quam Repudi culpa al minus at porro?ssitatibus laborum minu minus at porro?ssitatibus laborum minus at porro? culpas at porro? culpaias",
     },
     {
       id: 3,
       name: "Rabbitto",
-      img: { rabbit },
+      img:  rabbit ,
       description:
         "minus at porro?ssitatibus laborum minus at porro? culpaalias ullam. Necessitatibus, ut placeat i dolorum  assumenda, sint quam Repudi culpa al minus at porro?ssitatibus laborum minu minus at porro?ssitatibus laborum minus at porro? culpas at porro? culpaias",
     },
   ];
   return (
-    <>
-      <div class="card">
-        <div class="card-content">
-          <div class="back">
-            <div class="back-content">
-              <img src={cat} />
-              <strong>Hover Me</strong>
+    <div className="card-list">
+      {animals.map((animal) => (
+        <div className="card" key={animal.id}>
+          <div className="card-content">
+            <div className="back">
+              <div className="back-content">
+                <img src={animal.img} alt={animal.name} />
+                <strong>Hover Me</strong>
+              </div>
             </div>
-          </div>
-          <div class="front">
-            <div class="img">
-              <div class="circle"></div>
-              <div class="circle" id="right"></div>
-              <div class="circle" id="bottom"></div>
-            </div>
+            <div className="front">
+              <div className="img">
+                <div className="circle"></div>
+                <div className="circle" id="right"></div>
+                <div className="circle" id="bottom"></div>
+              </div>
 
-            <div class="front-content">
-              <small class="badge">Catto</small>
-              <div class="description">
-                <div class="title">
-                  <p class="title">
-                    <strong>
-                      minus at porro?ssitatibus laborum minus at porro? culpa
-                      alias ullam. Necessitatibus, ut placeat i dolorum
-                      assumenda, sint quam Repudi culpa alias
-                    </strong>
-                    <br />
-                    <strong>
-                      minus at porro?ssitatibus laborum minus at porro? culpa
-                      alias ullam. , ut placeat i dolorum assumenda, sint quam
-                      Repudi culpa alias
-                    </strong>
-                    <br />
-                    <strong>
-                      minus at porro? laborum minus at porro? Repudiandae culpa
-                      alias ullam. Necessitatibus, ut placeat i dolorum , sint
-                      quam Repudi culpa alias
-                    </strong>
-                  </p>
+              <div className="front-content">
+                <small className="badge">{animal.name}</small>
+                <div className="description">
+                  <div className="title">
+                    <p className="title">{animal.description}</p>
+                  </div>
+                  <p className="card-footer">30 Mins &nbsp; | &nbsp; 1 Serving</p>
                 </div>
-                <p class="card-footer">30 Mins &nbsp; | &nbsp; 1 Serving</p>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
+      ))}
+    </div>
+  
   );
 };
 
