2d428907287cc54443688e6b53ebf00f1daa5dcc
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-02-07 20:38:18 +0600

    landing page

--- src/components/Home/Footer/Footer.jsx
+++ src/components/Home/Footer/Footer.jsx
@@ -2,33 +2,36 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="pt-20">
       <p className="text-center">
         ACME Industries Ltd.
         <br />
         Providing reliable tech since 1992
       </p>
 
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </nav>
+      <div className="grid grid-cols-3 pt-20">
+        <nav>
+          <h6 className="font-bold text-black">Services</h6>
+          <a className="font-semibold text-black">Branding</a>
+          <br />
+          <a className="font-semibold text-black">Design</a> <br />
+          <a className="font-semibold text-black">Marketing</a> <br />
+          <a className="font-semibold text-black">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="font-bold text-black">Company</h6>
+          <a className="font-semibold text-black">About us</a> <br />
+          <a className="font-semibold text-black">Contact</a> <br />
+          <a className="font-semibold text-black">Jobs</a> <br />
+          <a className="font-semibold text-black">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="font-bold text-black">Legal</h6>
+          <a className="font-semibold text-black">Terms of use</a> <br />
+          <a className="font-semibold text-black">Privacy policy</a> <br />
+          <a className="font-semibold text-black">Cookie policy</a>
+        </nav>
+      </div>
     </div>
   );
 };
