205a664a6fbcc328719530249ebd988dc6f1c380
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-02-07 23:50:12 +0600

    contact

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Navbar/Navbar";
 import Card from "./Card/Card";
 import Footer from "./Footer/Footer";
+import Contact from "./Contact/Contact";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Card />
+      <Contact />
       <Footer />
     </div>
   );
