9e221f32884bb6252dec39ee4cb339d716fa640e
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-02-23 23:42:53 +0600

    service added

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Service = () => {
-    return (
-      <div>
-        <div class="loader">
-          <div class="box1"></div>
-          <div class="box2"></div>
-          <div class="box3"></div>
-        </div>
+  return (
+    <div>
+      <p>sjresediorrfjnmsdk,c</p>
+      <div class="loader bg-blue-700">
+        <div class="box1"></div>
+        <div class="box2"></div>
+        <div class="box3"></div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Service;
