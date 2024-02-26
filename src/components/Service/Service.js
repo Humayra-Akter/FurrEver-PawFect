45d5d62008703e3d30bbfa8b40c9c8ecf5609f93
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-02-26 12:24:14 +0600

    cross toggle added

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -19,6 +19,12 @@ const Service = () => {
         <div class="cell d-4"></div>
       </div>
       <button class="Btn"></button>
+       <input type="checkbox" id="checkbox"/>
+    <label for="checkbox" class="toggle">
+        <div class="bars" id="bar1"></div>
+        <div class="bars" id="bar2"></div>
+        <div class="bars" id="bar3"></div>
+    </label>
     </div>
   );
 };
