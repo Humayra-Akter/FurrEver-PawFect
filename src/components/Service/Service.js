b0d203554a47d0f4e1d1ce5ac5a6d1b9cd840d67
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-02-26 12:21:57 +0600

    button added

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -2,19 +2,23 @@ import React from "react";
 
 const Service = () => {
   return (
-    <div class="loader">
-      <div class="cell d-0"></div>
-      <div class="cell d-1"></div>
-      <div class="cell d-2"></div>
+    <div>
+      {" "}
+      <div class="loader">
+        <div class="cell d-0"></div>
+        <div class="cell d-1"></div>
+        <div class="cell d-2"></div>
 
-      <div class="cell d-1"></div>
-      <div class="cell d-2"></div>
+        <div class="cell d-1"></div>
+        <div class="cell d-2"></div>
 
-      <div class="cell d-2"></div>
-      <div class="cell d-3"></div>
+        <div class="cell d-2"></div>
+        <div class="cell d-3"></div>
 
-      <div class="cell d-3"></div>
-      <div class="cell d-4"></div>
+        <div class="cell d-3"></div>
+        <div class="cell d-4"></div>
+      </div>
+      <button class="Btn"></button>
     </div>
   );
 };
