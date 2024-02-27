569b45b6fa448409d571324ff7197f6e0bf4ac8e
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-02-27 23:46:17 +0600

    input

---
+++ src/components/Service/Input.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Input = () => {
+    return (
+        <div>
+            <input type="text" autocomplete="off" name="text" class="input" placeholder="Username"></input>
+        </div>
+    );
+};
+
+export default Input;
