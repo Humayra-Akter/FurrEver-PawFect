8c6e057c14764df7821791e83b4396354a84be39
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-02-29 22:46:29 +0600

    input

--- src/components/Service/Input.js
+++ src/components/Service/Input.js
@@ -3,9 +3,25 @@ import './Input.css'
 
 const Input = () => {
     return (
-        <div>
-            <input type="text" autocomplete="off" name="text" class="input" placeholder="Username"></input>
+      <div>
+        <input
+          type="text"
+          autocomplete="off"
+          name="text"
+          class="input"
+          placeholder="Username"
+        ></input>
+
+        <div class="loader">
+          <div class="wrapper">
+            <div class="circle"></div>
+            <div class="line-1"></div>
+            <div class="line-2"></div>
+            <div class="line-3"></div>
+            <div class="line-4"></div>
+          </div>
         </div>
+      </div>
     );
 };
 
