f2f8aa75c488e2e2ea147747fa2b60e13f5b9c9d
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-05-08 15:02:34 +0600

    about addd

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,69 +1,77 @@
 import React from "react";
-import banner6 from "../../images/cat-dog.png";
-import banner1 from "../../images/catt-dog.png";
-import banner3 from "../../images/cat-dogg.png";
+import bg from "../../images/about-bg.jpg";
 
 const About = () => {
   return (
     <div className="mt-20">
-      <h1 className="text-3xl font-bold text-center text-secondary my-8">
+      <h1 className="text-3xl font-bold font-mono text-center text-secondary my-8">
         About - "FurrEver PawFect"
       </h1>
       <div className="grid lg:grid-cols-2 gap-8 p-8">
         <div
           style={{
-            background: `url(${banner6})`,
+            background: `url(${bg})`,
             backgroundSize: "cover",
           }}
           className="bg-accent rounded-lg p-6 flex flex-col items-center justify-center text-center"
         >
-          <h2 className="text-xl font-bold text-black mb-4">Our Mission</h2>
-          <p className="text-sm font-semibold text-justify text-primary">
-            To bridge artisans and discerning buyers seeking exceptional
-            handcrafted goods. We're committed to preserving and promoting
-            traditional craftsmanship and nurturing a global community that
-            values artistry and authenticity.
+          <h2 className="text-xl font-bold text-white  font-mono mb-4">
+            Our Mission
+          </h2>
+          <p className="text-sm font-semibold text-justify font-mono text-gray-400">
+            At FurrEver-PawFect, our mission is to foster a world where every
+            pet receives the love, care, and respect they deserve. We're
+            dedicated to connecting pets with loving homes, providing essential
+            resources for responsible pet ownership, and building a vibrant
+            community of passionate pet enthusiasts.
           </p>
-          <h2 className="text-xl font-bold text-black mb-4">
+          <h2 className="text-xl font-bold font-mono text-white my-4">
             Global & Local Impact
           </h2>
-          <p className="text-sm font-semibold text-primary text-justify">
-            We understand the importance of both local and global impact. We
-            celebrate local artisans and their culture while creating a global
-            marketplace for handcrafted treasures, connecting the world through
-            artistry.
+          <p className="text-sm font-mono font-semibold text-gray-400 text-justify">
+            At FurrEver-PawFect, our mission is to uphold the highest standards
+            of quality and authenticity in every interaction, service, and
+            resource we provide. We are dedicated to ensuring the well-being of
+            pets and fostering genuine connections between animals and their
+            human companions, both locally and globally.
           </p>
         </div>
         <div className="grid lg:grid-cols-2 gap-4">
           <div
             style={{
-              background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${banner3})`,
+              background: `url(${bg})`,
               backgroundSize: "cover",
             }}
             className="bg-accent rounded-lg p-6 hover:scale-105 hover:bg-secondary hover:shadow-2xl"
           >
-            <h2 className="text-xl font-bold text-neutral mb-4">
+            <h2 className="text-xl font-mono text-center font-bold text-white mb-4">
               Quality & Authenticity
             </h2>
-            <p className="text-sm font-semibold text-accent text-justify">
-              We curate a collection of handcrafted products that epitomize
-              quality and authenticity. Each item is carefully selected,
-              representing the very essence of craftsmanship.
+            <p className="text-sm font-mono font-semibold text-gray-400 text-justify">
+              We strive to empower individuals to make a positive impact on the
+              lives of pets, one adoption, one caretaker, and one community at a
+              time.Through our commitment to global and local initiatives, we
+              aim to create a future where every pet thrives in a nurturing
+              environment and every owner experiences the joy of unconditional
+              companionship.
             </p>
           </div>
           <div
             style={{
-              background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${banner1})`,
+              background: `url(${bg})`,
               backgroundSize: "cover",
             }}
             className="bg-accent rounded-lg p-6 hover:scale-105 hover:bg-secondary hover:shadow-2xl"
           >
-            <h2 className="text-xl font-bold text-neutral mb-4">Our Vision</h2>
-            <p className="text-sm font-semibold text-accent text-justify">
-              To be a beacon of craftsmanship, a trusted platform for artisans,
-              and a source of inspiration for all lovers of handcrafted beauty.
-              We provide an unparalleled shopping experience founded on quality,
-              authenticity, and the celebration of human creativity.
+            <h2 className="text-xl font-mono font-bold text-white text-center mb-4">
+              Our Vision
+            </h2>
+            <p className="text-sm font-mono font-semibold text-gray-400 text-justify">
+              Our vision is to create a future where no pet is left behind. We
+              envision a world where every pet has a safe and loving home, every
+              owner is equipped with the knowledge and resources to provide
+              optimal care, and every community works together to ensure the
+              well-being of all animals.
             </p>
           </div>
         </div>
