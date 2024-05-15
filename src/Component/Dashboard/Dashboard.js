4c2c9e5ef295c1660be7389e47e0c770dcdcadde
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-05-15 23:18:49 +0600

    dashboard

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -1,124 +1,120 @@
-import React from 'react';
+import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
-
 const Dashboard = () => {
-    return (
-      <div
-        
-        className="drawer lg:drawer-open"
-      >
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center">
-          <h1 className="w-full h-full font-extrabold text-left text-5xl text-blue-700 pl-4 pt-7">
-            Dashboard
-          </h1>
-          <Outlet />
-          <label
-            for="my-drawer-2"
-            className="btn btn-primary drawer-button lg:hidden"
-          >
-            Open drawer
-          </label>
-        </div>
-        <div className="drawer-side">
-          <label for="my-drawer-2" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 h-full bg-blue-50 text-base-content">
-            <li>
-              <Link
-                to="/dashboard"
-                className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full pt-20 uppercase h-full font-bold text-center text-sm text-blue-700"
-              >
-                👉 Admin
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/dashboard/customers"
-                className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-blue-700"
-              >
-                👉 customers
-              </Link>
-            </li>
+  return (
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        <h1 className="w-full h-full font-extrabold text-left text-5xl font-mono text-secondary pl-4 pt-7">
+          Dashboard
+        </h1>
+        <Outlet />
+        <label
+          for="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label for="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-60 h-full bg-primary text-base-content">
+          <li>
+            <Link
+              to="/dashboard"
+              className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full pt-20 uppercase h-full font-bold text-center text-sm text-secondary font mono "
+            >
+              👉 Admin
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/dashboard/customers"
+              className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-secondary font mono "
+            >
+              👉 customers
+            </Link>
+          </li>
 
-            <li>
-              <Link
-                to="/dashboard/staff"
-                className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-blue-700"
-              >
-                👉 staff
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/dashboard/vet"
-                className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-blue-700"
-              >
-                👉 veterinarian
-              </Link>
-            </li>
+          <li>
+            <Link
+              to="/dashboard/staff"
+              className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-secondary font mono "
+            >
+              👉 staff
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/dashboard/vet"
+              className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-secondary font mono "
+            >
+              👉 veterinarian
+            </Link>
+          </li>
 
-            {/* <li>
+          {/* <li>
             <Link
               to="/dashboard/healthRecord"
-              className="w-full uppercase h-full font-bold text-center text-sm text-blue-700"
+              className="w-full uppercase h-full font-bold text-center text-sm text-secondary font mono "
             >
               👉 health record
             </Link>
           </li> */}
 
-            <li>
-              <Link
-                to="/dashboard/donation"
-                className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-blue-700"
-              >
-                👉 donation
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/dashboard/feedback"
-                className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-blue-700"
-              >
-                👉 feedback
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/dashboard/rescuer"
-                className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-blue-700"
-              >
-                👉 rescuer
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/dashboard/cabin"
-                className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-blue-700"
-              >
-                👉 cabin
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-blue-700"
-                to="/dashboard/daycareAnimal"
-              >
-                👉 daycare Animal
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-blue-700"
-                to="/dashboard/rescuedAnimal"
-              >
-                👉 rescued Animal
-              </Link>
-            </li>
-          </ul>
-        </div>
+          <li>
+            <Link
+              to="/dashboard/donation"
+              className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-secondary font mono "
+            >
+              👉 donation
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/dashboard/feedback"
+              className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-secondary font mono "
+            >
+              👉 feedback
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/dashboard/rescuer"
+              className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-secondary font mono "
+            >
+              👉 rescuer
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/dashboard/cabin"
+              className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-secondary font mono "
+            >
+              👉 cabin
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-secondary font mono "
+              to="/dashboard/daycareAnimal"
+            >
+              👉 daycare Animal
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-full uppercase h-full font-bold text-center text-sm text-secondary font mono "
+              to="/dashboard/rescuedAnimal"
+            >
+              👉 rescued Animal
+            </Link>
+          </li>
+        </ul>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Dashboard;
