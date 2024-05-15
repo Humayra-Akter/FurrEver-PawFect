e08e30e140a19ebae98ef3cc0b243d553db84c59
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-05-15 23:21:55 +0600

    services

--- src/Component/Services/Services.js
+++ src/Component/Services/Services.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
 const Services = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="pt-14 gap-10">
+        <div className="text-center">
+          <h3 className="text-secondary font-mono font-black text-2xl">
+            OUR SERVICES
+          </h3>
         </div>
-    );
+        <div className="grid grid-cols-3 px-20 pt-20 gap-10 "></div>
+        
+      </div>
+    </div>
+  );
 };
 
 export default Services;
