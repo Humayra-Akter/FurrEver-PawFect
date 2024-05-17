18feda589139c987ec34492761314e2d8a76e986
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-05-17 17:03:02 +0600

    register

--- src/App.js
+++ src/App.js
@@ -27,6 +27,21 @@ function App() {
         <Route path="/feedback" element={<Feedback />} />
         <Route path="/donation" element={<Donation />} />
         <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route path="dashboard" element={<Dashboard />}>
+          {/* <Route index element={<Admin />}></Route>
+          <Route path="staff" element={<Staff />}></Route>
+          <Route path="vet" element={<Vet />}></Route>
+          <Route path="customers" element={<Customers />}></Route>
+          <Route path="rescuer" element={<Rescuer />}></Route>
+          <Route path="healthRecord" element={<HealthRecord />}></Route>
+          <Route path="cabin" element={<Cabin />}></Route>
+          <Route path="donation" element={<Donation />}></Route>
+          <Route path="feedback" element={<Feedback />}></Route>
+          <Route path="daycareAnimal" element={<DaycareAnimal />}></Route>
+          <Route path="rescuedAnimal" element={<RescuedAnimal />}></Route> */}
+        </Route>
+
         <Route path="*" element={<Error />}></Route>
       </Routes>
       <ToastContainer />
