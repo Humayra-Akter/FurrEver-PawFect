27af14e253df90b7dbc8b18d2ce2421576bd2f2a
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-05-22 00:39:06 +0600

    blog1 alignment added

--- src/Component/Donation/Blog1.js
+++ src/Component/Donation/Blog1.js
@@ -2,113 +2,227 @@ import React from "react";
 
 const Blog1 = () => {
   return (
-    <div>
+    <div className="my-20 mx-auto max-w-6xl font-mono text-justify">
+      <h1 className="text-center text-secondary mb-7 text-2xl font-bold">
+        How Your Donations Help
+      </h1>
       Best practices for animal care ensure the well-being, health, and
       happiness of animals. These practices vary depending on the type of
       animal, but some general principles apply universally. Here are some best
       practices for animal care:
       <div>
-        ### General Best Practices{" "}
+        <h1 className="text-center text-secondary my-7 text-xl font-bold">
+          General Best Practices{" "}
+        </h1>
         <ul>
-          <li>
+          <li className="my-7">
             {" "}
-            1. **Proper Nutrition** - Provide a balanced diet appropriate for
-            the specific species, age, and health needs of the animal. - Ensure
-            constant access to fresh water.
+            <strong className="text-white text-md">
+              1. **Proper Nutrition**
+            </strong>{" "}
+            <li>
+              - Provide a balanced diet appropriate for the specific species,
+              age, and health needs of the animal.
+            </li>{" "}
+            <li> - Ensure constant access to fresh water.</li>
           </li>
-          <li>
-            2. **Adequate Shelter** - Provide shelter that protects from extreme
-            weather conditions and predators. - Ensure the living environment is
-            clean, spacious, and comfortable.
-          </li>{" "}
-          <li>
-            <strong> 3. **Regular Health Care**</strong> - Schedule regular
-            veterinary check-ups. - Keep vaccinations up-to-date and follow
-            preventive measures for parasites. - Monitor for signs of illness or
-            injury and seek prompt veterinary care if needed.
+          <li className="my-7">
+            <strong className="text-white text-md">
+              {" "}
+              2. **Adequate Shelter**{" "}
+            </strong>{" "}
+            <li>
+              {" "}
+              - Provide shelter that protects from extreme weather conditions
+              and predators.{" "}
+            </li>
+            <li>
+              - Ensure the living environment is clean, spacious, and
+              comfortable.
+            </li>
+          </li>
+          <li className="my-7">
+            <strong className="text-white text-md">
+              {" "}
+              3. **Regular Health Care**
+            </strong>{" "}
+            <li>- Schedule regular veterinary check-ups.</li>
+            <li>
+              {" "}
+              - Keep vaccinations up-to-date and follow preventive measures for
+              parasites.{" "}
+            </li>
+            <li>
+              - Monitor for signs of illness or injury and seek prompt
+              veterinary care if needed.
+            </li>
           </li>
           {"  "}
-          <li>
-            <strong>4. **Physical and Mental Stimulation**</strong> - Provide
-            opportunities for exercise and play to promote physical health and
-            mental well-being. - Use toys, puzzles, and social interaction to
-            keep the animal mentally stimulated.{" "}
+          <li className="my-7">
+            <strong className="text-white text-md">
+              4. **Physical and Mental Stimulation**
+            </strong>{" "}
+            <li>
+              - Provide opportunities for exercise and play to promote physical
+              health and mental well-being.
+            </li>{" "}
+            <li>
+              {" "}
+              - Use toys, puzzles, and social interaction to keep the animal
+              mentally stimulated.{" "}
+            </li>
           </li>
-          <li>
-            <strong>5. **Proper Grooming**</strong> - Maintain regular grooming
-            to keep the animal clean and healthy, which may include bathing,
-            brushing, and nail trimming. - Regularly check and clean ears,
-            teeth, and eyes.
+          <li className="my-7">
+            <strong className="text-white text-md">
+              5. **Proper Grooming**
+            </strong>{" "}
+            <li>
+              {" "}
+              - Maintain regular grooming to keep the animal clean and healthy,
+              which may include bathing, brushing, and nail trimming.
+            </li>{" "}
+            <li> - Regularly check and clean ears, teeth, and eyes.</li>
           </li>
         </ul>
       </div>
       <div>
-        ### Species-Specific Practices{" "}
+        {" "}
+        <h1 className="text-center text-secondary my-7 text-xl font-bold">
+          Species-Specific Practices{" "}
+        </h1>
         <ul>
-          <li>
-            <strong> #### Dogs </strong>- Ensure regular walks and playtime for
-            exercise. - Socialize with other dogs and humans to build positive
-            behaviors. - Provide obedience training to encourage good manners
-            and mental engagement.
+          <li className="my-7">
+            <strong className="text-white text-md"> #### Dogs </strong>
+            <li>- Ensure regular walks and playtime for exercise.</li>
+            <li>
+              {" "}
+              - Socialize with other dogs and humans to build positive
+              behaviors.{" "}
+            </li>
+            <li>
+              - Provide obedience training to encourage good manners and mental
+              engagement.
+            </li>
           </li>{" "}
-          <li>
+          <li className="my-7">
             {" "}
-            <strong>#### Cats</strong> - Provide scratching posts to satisfy
-            natural scratching behavior and protect furniture. - Ensure a litter
-            box is always clean and accessible. - Create vertical spaces like
-            cat trees for climbing and observing.
+            <strong className="text-white text-md">#### Cats</strong>
+            <li>
+              {" "}
+              - Provide scratching posts to satisfy natural scratching behavior
+              and protect furniture.
+            </li>{" "}
+            <li>- Ensure a litter box is always clean and accessible.</li>
+            <li>
+              {" "}
+              - Create vertical spaces like cat trees for climbing and
+              observing.
+            </li>
           </li>{" "}
-          <li>
-            <strong>#### Birds </strong>- Provide a spacious cage with room for
-            flying. - Offer a variety of perches and toys for stimulation. -
-            Include fresh fruits, vegetables, and a balanced seed or pellet
-            diet.
+          <li className="my-7">
+            <strong className="text-white text-md">#### Birds </strong>
+            <li>- Provide a spacious cage with room for flying. </li>
+            <li>- Offer a variety of perches and toys for stimulation. </li>
+            <li>
+              - Include fresh fruits, vegetables, and a balanced seed or pellet
+              diet.
+            </li>
           </li>{" "}
-          <li>
+          <li className="my-7">
             {" "}
-            <strong>
+            <strong className="text-white text-md">
               #### Small Mammals (Rabbits, Guinea Pigs, Hamsters)
             </strong>{" "}
-            - Provide hiding places and tunnels to mimic natural burrowing
-            behavior. - Ensure a diet high in fiber, such as hay, and
-            supplemented with fresh vegetables. - Maintain clean bedding and
-            habitat.
+            <li>
+              - Provide hiding places and tunnels to mimic natural burrowing
+              behavior.{" "}
+            </li>
+            <li>
+              - Ensure a diet high in fiber, such as hay, and supplemented with
+              fresh vegetables.
+            </li>{" "}
+            <li> - Maintain clean bedding and habitat.</li>
           </li>{" "}
-          <li>
+          <li className="my-7">
             {" "}
-            <strong> #### Reptiles and Amphibians</strong> - Provide proper
-            lighting and heating to mimic natural habitats and regulate body
-            temperature. - Ensure the enclosure has hiding spots and climbing
-            opportunities. - Maintain appropriate humidity levels and clean
-            water for hydration.{" "}
+            <strong className="text-white text-md">
+              {" "}
+              #### Reptiles and Amphibians
+            </strong>{" "}
+            <li>
+              {" "}
+              - Provide proper lighting and heating to mimic natural habitats
+              and regulate body temperature.
+            </li>{" "}
+            <li>
+              - Ensure the enclosure has hiding spots and climbing
+              opportunities.
+            </li>
+            <li>
+              - Maintain appropriate humidity levels and clean water for
+              hydration.{" "}
+            </li>
           </li>
         </ul>
       </div>
       <div>
-        ### Ethical Considerations
+        {" "}
+        <h1 className="text-center text-secondary my-7 text-xl font-bold">
+          Ethical Considerations
+        </h1>
         <ul>
-          <li>
-            1. **Responsible Ownership** - Research thoroughly before acquiring
-            a pet to understand the commitment and care required. - Adopt from
-            shelters or rescue organizations when possible.
+          <li className="my-7">
+            <strong className="text-white text-md">
+              1. **Responsible Ownership**
+            </strong>
+            <li>
+              {" "}
+              - Research thoroughly before acquiring a pet to understand the
+              commitment and care required.
+            </li>{" "}
+            <li>
+              {" "}
+              - Adopt from shelters or rescue organizations when possible.
+            </li>
           </li>
-          <li>
+          <li className="my-7">
             {" "}
-            2. **Humane Treatment** - Avoid practices that cause unnecessary
-            stress or harm. - Train using positive reinforcement techniques
-            rather than punishment.
+            <strong className="text-white text-md">
+              2. **Humane Treatment**
+            </strong>
+            <li> - Avoid practices that cause unnecessary stress or harm. </li>
+            <li>
+              - Train using positive reinforcement techniques rather than
+              punishment.
+            </li>
           </li>{" "}
-          <li>
+          <li className="my-7">
             {" "}
-            3. **Emergency Preparedness** - Have a plan in place for
-            emergencies, including natural disasters, that ensures the safety of
-            the animal. - Keep a first aid kit and emergency contact numbers
-            accessible.
+            <strong className="text-white text-md">
+              3. **Emergency Preparedness**{" "}
+            </strong>
+            <li>
+              - Have a plan in place for emergencies, including natural
+              disasters, that ensures the safety of the animal.{" "}
+            </li>
+            <li>
+              - Keep a first aid kit and emergency contact numbers accessible.
+            </li>
           </li>{" "}
-          <li>
-            ### Legal and Regulatory Compliance - Adhere to local and national
-            regulations regarding animal care and welfare. - Stay informed about
-            changes in animal care standards and best practices.{" "}
+          <li className="my-7">
+            {" "}
+            <strong className="text-white text-md">
+              ### Legal and Regulatory Compliance
+            </strong>
+            <li>
+              - Adhere to local and national regulations regarding animal care
+              and welfare.
+            </li>{" "}
+            <li>
+              {" "}
+              - Stay informed about changes in animal care standards and best
+              practices.{" "}
+            </li>
           </li>
         </ul>
       </div>
