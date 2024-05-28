f791034c660f2d3dadaee3d1ea7d3bbeccebbe8f
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-05-28 23:39:45 +0600

    cust appointment

--- src/Component/Dashboard/DashboardCustomer/Appointments.js
+++ src/Component/Dashboard/DashboardCustomer/Appointments.js
@@ -46,155 +46,161 @@ const Appointments = () => {
           </div>
         </div>
       </section>
-      <div
-        style={{
-          background: `url(${bg})`,
-          backgroundSize: "cover",
-        }}
-        className="border-2 border-gray-700 bg-white max-w-xl rounded p-2 shadow-md"
-      >
-        <h2 className="text-xl font-mono font-bold text-secondary mb-4">
-          Book an Appointments
-        </h2>
-        <form className="space-y-4">
-          <div>
-            <label
-              htmlFor="name"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="date"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Appointment Date
-            </label>
-            <input
-              type="date"
-              id="date"
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="time"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Appointment Time
-            </label>
-            <input
-              type="time"
-              id="time"
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="service"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Service
-            </label>
-            <select
-              id="service"
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            >
-              <option>General Check-up</option>
-              <option>Vaccination</option>
-              <option>Grooming</option>
-              <option>Daycare</option>
-              <option>Other</option>
-            </select>
-          </div>
-          <button
-            type="submit"
-            className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700"
-          >
-            Book Appointment
-          </button>
-        </form>
-      </div>
+      <div className="grid grid-cols-2 mt-16 gap-7">
+        <div
+          style={{
+            background: `url(${bg})`,
+            backgroundSize: "cover",
+          }}
+          className="border-2 border-gray-700 bg-white max-w-xl rounded p-4 shadow-md"
+        >
+          <h2 className="text-xl font-mono font-bold text-secondary mb-4">
+            Book an Appointments
+          </h2>
+          <form className="space-y-4">
+            <div>
+              <label
+                htmlFor="name"
+                className="font-semibold mb-2 font-mono block"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="mt-1 block w-full bg-gray-400 border text-black border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="font-semibold mb-2 font-mono bloc"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="mt-1 block w-full border bg-gray-400 text-black border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="date"
+                className="font-semibold mb-2 font-mono block"
+              >
+                Appointment Date
+              </label>
+              <input
+                type="date"
+                id="date"
+                className="mt-1 block w-full border bg-gray-400 border-gray-300 rounded-md shadow-sm p-2 text-black focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="time"
+                className="font-semibold mb-2 font-mono block"
+              >
+                Appointment Time
+              </label>
+              <input
+                type="time"
+                id="time"
+                className="mt-1 block w-full border bg-gray-400 border-gray-300 rounded-md shadow-sm p-2 text-black focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="service"
+                className="font-semibold mb-2 font-mono block"
+              >
+                Service
+              </label>
+              <select
+                id="service"
+                className="mt-1 block w-full border bg-gray-400 border-gray-300 rounded-md shadow-sm p-2 text-black focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              >
+                <option>General Check-up</option>
+                <option>Vaccination</option>
+                <option>Grooming</option>
+                <option>Daycare</option>
+                <option>Other</option>
+              </select>
+            </div>
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="bg-primary hover:bg-secondary mt-3 hover:text-primary text-secondary font-mono font-bold py-2 px-4 rounded"
+              >
+                Book Appointment
+              </button>
+            </div>
+          </form>
+        </div>
 
-      {/* Section for Contacting Support */}
-     <div
-        style={{
-          background: `url(${bg})`,
-          backgroundSize: "cover",
-        }}
-        className="border-2 border-gray-700 bg-white max-w-xl rounded p-2 shadow-md"
-      >
-        <h2 className="text-xl font-mono font-bold text-secondary mb-4">
-          Book an Appointments
-        </h2>
-        <h2 className="text-2xl font-semibold mb-4">Need Help?</h2>
-        <form className="space-y-4">
-          <div>
-            <label
-              htmlFor="contact-name"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              id="contact-name"
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="contact-email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="contact-email"
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="contact-message"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Message
-            </label>
-            <textarea
-              id="contact-message"
-              rows="4"
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700"
-          >
-            Send Message
-          </button>
-        </form>
+        {/* Section for Contacting Support */}
+        <div
+          style={{
+            background: `url(${bg})`,
+            backgroundSize: "cover",
+          }}
+          className="border-2 border-gray-700 bg-white max-w-xl rounded p-4 shadow-md"
+        >
+          <h2 className="text-xl font-mono font-bold text-secondary mb-4">
+            Need Help?
+          </h2>
+          <form className="space-y-4">
+            <div>
+              <label
+                htmlFor="contact-name"
+                className="font-semibold mb-2 font-mono block"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="contact-name"
+                className="mt-1 block w-full border  bg-gray-400 border-gray-300 rounded-md shadow-sm p-2 text-black focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="contact-email"
+                className="font-semibold mb-2 font-mono block"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="contact-email"
+                className="mt-1 block w-full border bg-gray-400 border-gray-300 rounded-md shadow-sm p-2 text-black focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="contact-message"
+                className="font-semibold mb-2 font-mono block"
+              >
+                Message
+              </label>
+              <textarea
+                id="contact-message"
+                rows="4"
+                className="mt-1 block w-full border bg-gray-400 border-gray-300 rounded-md shadow-sm p-2 text-black focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              ></textarea>
+            </div>
+
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="bg-primary hover:bg-secondary mt-3 hover:text-primary text-secondary font-mono font-bold py-2 px-4 rounded"
+              >
+                Send Message
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
